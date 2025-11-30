99d158a91406c9dd05cf73759eea7de16c2751c9
essorcal-ball <esamwoodman@outlook.com>
2025-11-30 16:05:13 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Serve client folder
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(require("express-fileupload")());
 
 // API routes
 app.use("/api", require("./routes"));
 app.use("/api/admin", require("./routes-admin"));
 
-// For any non-API page, send index.html
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../client")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on Port " + PORT));
+app.listen(PORT, () => console.log("Server running on port " + PORT));
