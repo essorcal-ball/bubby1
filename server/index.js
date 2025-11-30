588b7635381ca42dd4c3864404f9b4212bba0b4f
essorcal-ball <esamwoodman@outlook.com>
2025-11-30 14:31:22 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,3 +23,24 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () =>
   console.log("Server running on port", PORT)
 );
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve client files
+app.use(express.static(path.join(__dirname, "../client")));
+
+// your API routes
+app.use("/api", require("./routes"));
+app.use("/api/admin", require("./routes-admin"));
+
+// for any non-API route, serve index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
